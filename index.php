379e344a0d5a6b4c96a6165081a7876772038aac
Chidiebere Okpara <chididokpara@gmail.com>
2024-04-05 00:01:28 -0700

    navbar changes
    ...

--- index.php
+++ index.php
@@ -16,48 +16,45 @@ require_once("./php/config.php");
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="./styles/navbar.css">
     <link rel="stylesheet" href="./styles/style.css">
+    <link rel="shortcut icon" href="../imgs/favicon.ico" />
 </head>
 <body>
 <nav class="topnav navbar bg-dark fixed-top navbar-expand-lg bg-body-tertiary border-bottom border-body" data-bs-theme="dark">
   <div class="container-md">
-    <a class="navbar-brand" href="#">The Movie Buffs</a>
+    <a class="navbar-brand" href="#"><img alt="The Movie Buffs" src="./imgs/moviebuffs.png" class="mb-nav-logo"/>&nbsp;The MovieBuffs</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Lists</a>
-        </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Best Movies
+          <a class="nav-link " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Movies
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">All </a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Latest Releases</a></li>
+            <li><a class="dropdown-item" href="#">Best Movies All Time </a></li>
+            <li><a class="dropdown-item" href="#">Best Movies This Year  </a></li>
+            <li><a class="dropdown-item" href="#">Best Action Movies</a></li>
+            <li><a class="dropdown-item" href="#">Best Comedy Movies</a></li>
+            <li><a class="dropdown-item" href="#">Best Horror Movies</a></li>
           </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+      <form class="d-flex topnav-search-box-can" role="search">
+        <input class="form-control me-2 topnav-search-box" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-secondary topnav-search-btn" type="submit"><img alt=".." src="./imgs/search_icon.png" class="search-icon"/></button>
       </form>
+      <button class="btn register-btn">LOGIN</button>
     </div>
   </div>
 </nav>
-<br>
-<br>
-<br>
+<div class="page-contents container-md">
 <h1>The Movie Buffs</h1>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
