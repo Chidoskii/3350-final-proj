18c1412c66147d894e7adc82d9edda328109de11
Chidiebere Okpara <chididokpara@gmail.com>
2024-04-05 00:11:35 -0700

    ...

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require_once("./php/config.php");
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title><?= $PROJECT_NAME ?></title>
+    <title><?= $PROJECT_NAME ?> | Movie Reviews and Ratings</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,400;0,600;0,700;1,600&display=swap" rel="stylesheet">
