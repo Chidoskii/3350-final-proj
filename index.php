1c799aee28a924d9193873f9b38a5604977f6f8b
Chidiebere Okpara <chididokpara@gmail.com>
2024-04-05 23:18:45 -0700

    footer and page-title

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ require_once("./php/config.php");
 <body>
 <nav class="topnav navbar bg-dark fixed-top navbar-expand-lg bg-body-tertiary border-bottom border-body" data-bs-theme="dark">
   <div class="container-md">
-    <a class="navbar-brand" href="#"><img alt="The Movie Buffs" src="./imgs/moviebuffs.png" class="mb-nav-logo"/>&nbsp;The MovieBuffs</a>
+    <a class="navbar-brand" href="#"><img alt="The Movie Buffs" src="./imgs/moviebuffs.png" class="mb-nav-logo"/>&nbsp;TheMovieBuffs</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -53,8 +53,16 @@ require_once("./php/config.php");
   </div>
 </nav>
 <div class="page-contents container-md">
-<h1>The Movie Buffs</h1>
+  <div class="home-page-show-title-can">
+    <h1 class="home-page-show-header">Movies</h1>
+    <div class="cheeky-hook">Ratings and reviews from the best critics in the business </div>
+  </div>
 </div>
+
+
+<?php
+  require_once("./pages/footer.php");
+?>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
---
+++ pages/footer.php
@@ -0,0 +1,38 @@
+<footer>
+
+  <div class="container-fluid footy-can">
+    <div class="footy-header"><img alt="The Movie Buffs" src="./imgs/moviebuffs.png" class="mb-nav-logo"/>&nbsp;TheMovieBuffs</div>
+    <div class="footer-links-can">
+      <div class="footy-links-l">
+        <p class="footy-sub-header">Overview</p>
+        <a class="footy-link-list" href="#">Careers</a>
+        <a class="footy-link-list" href="#">Terms of Use</a>
+        <a class="footy-link-list" href="#">For Developers</a>
+        <a class="footy-link-list" href="#">Don't Sell My Personal Info </a>
+      </div>
+      <div class="footy-links-m">
+        <p class="footy-sub-header">Connect With Us</p>
+        <div class="socials-box container">
+        <a class="footy-link-list" href="#"><img alt="..." src="https://cdn.icon-icons.com/icons2/818/PNG/512/Social-35-Linkedin-Outline_icon-icons.com_66384.png" class="footy-social-icon"/></a>
+        <a class="footy-link-list" href="https://github.com/Chidoskii/TheMovieBuffs" target="_blank" rel="noopener noreferrer" ><img alt="..." src="https://cdn.icon-icons.com/icons2/510/PNG/512/social-github-outline_icon-icons.com_50020.png" class="footy-social-icon"/></a>
+        <a class="footy-link-list" href="#"><img alt="..." src="https://cdn.icon-icons.com/icons2/510/PNG/512/social-twitter-outline_icon-icons.com_49997.png" class="footy-social-icon"/></a>
+        </div>
+      </div>
+      <div class="footy-links-r">
+        <p class="footy-sub-header">Customer Support</p>
+        <a class="footy-link-list" href="#">Your Account</a>
+        <a class="footy-link-list" href="#">Help Center</a>
+        <a class="footy-link-list" href="#">Contact Us</a>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      &copy;&nbsp;
+      <div class="moviebuffs-copyright">
+      TheMovieBuffs
+      </div>
+      &nbsp;
+      2024. All Rights Reserved.
+    </div>
+  </div>
+
+</footer>
