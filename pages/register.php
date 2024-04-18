1e2d20b4a359eaeb036737d97f3c79b3a2d45b96
LogWB <rpf572@gmail.com>
2024-04-18 14:07:12 -0700

    Updated register.php to v1.1 and updated script.js

--- pages/register.php
+++ pages/register.php
@@ -11,29 +11,33 @@ if(DEVELOPER) {
   ini_set("display_errors", 1);
   ini_set("error_log", LOG_FILE);
 }*/
-if(isset($_POST['Register'])) {
+//if(isset($_POST['Register'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') { // Sends as a server request instead of isset
     $register_user = $_POST['register_user'];
     $register_email = $_POST['register_email'];
     $register_password = $_POST['register_password'];
 
     if (strlen($register_user) == 0 || strlen($register_email) == 0 || strlen($register_password) == 0) {
+        http_response_code(400);
         $_SESSION["register_error"] = "Missing info in required fields.";
-        //echo "<br>Empty\n";
+        echo "Empty request, something went wrong while sending";
         die();
     }
     else {
         inputFilter($register_user, $register_email, $register_password); // Sanitizes the string
         if(findUser($register_user) === $register_user) {
             //error
+            http_response_code(400);
             $_SESSION["register_error"] = "$register_user has been taken already";
-            //echo "<br>User already Exists";
+            echo "User already exists";
             die();
         }
         $check = findEmail($register_email);
         if ($check === $register_email) {
             //error
+            http_response_code(400);
             $_SESSION["register_error"] = "This email has already been registered";
-            //echo "<br>Email already Exists";
+            echo "Email already exists";
             die();
         }
         
@@ -49,11 +53,11 @@ if(isset($_POST['Register'])) {
         if ($check === $register_user) {
             //Success
             $_SESSION["user_added"] = "Welcome $register_user";
-            //echo "<br>User Added";
+            echo "User successfuly added";
         }
         else {
-            die();
-            //echo "<br>Failed to add";
+            http_response_code(405);
+            echo "Failed to add";
         }
     }
 }
@@ -91,34 +95,3 @@ function findEmail($register_email) {
     return $value;
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= $PROJECT_NAME . " | " . TITLE ?></title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,400;0,600;0,700;1,600&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href=<?= CSS ?>>
-    <link rel="shortcut icon" href="" />
-</head>
-    <body>
-           <h1>Write some code</h1>
-        <!-- <form method="post">
-            <label for="uname" class="form-label creds-label">Username</label><br>
-            <input type="text" id="uname" name="register_user" class="form-control creds-input-field"><br>
-            <label for="email" class="form-label creds-label">Email</label><br>
-            <input type="text" id="email" name="register_email" class="form-control creds-input-field"><br>
-            <label for="psswd" class="form-label creds-label">Password</label><br>
-            <input type="text" id="psswd" name="register_password" class="form-control creds-input-field">
-        <br>
-         <div class="creds-btns-can">
-          <input type="submit" name="Register" value="Register" class="btn btn-primary creds-form-btns creds-form-confirm"> -->
-</form> 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-    </body>
-</html>
--- scripts/script.js
+++ scripts/script.js
@@ -32,6 +32,38 @@ function closeOffcanvas() {
   backdrop.classList.remove('show');
 }
 
+// Function to register User
+function registerUser() {
+  //alert('wOrKs'); //Call test
+  var uname = document.getElementById("uname").value;
+  var email = document.getElementById("email").value;
+  var psswd = document.getElementById("psswd").value;
+  /*if (!uname || !email || !psswd) {
+    alert(psswd);
+    alert("Empty String");
+  }
+  else {*/
+   //Makes an object to send
+   let formData = new FormData();
+
+   formData.append('register_user', uname);
+   formData.append('register_email', email);
+   formData.append('register_password', psswd);
+
+   fetch('../pages/register.php', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => response.text()) // Sends object (Will request updating to JSON for security reasons)
+  .then(data => console.log(data))
+  .catch(error => console.error('Error: ', error));
+  //}
+}
+
+function loginUser() {
+  //alert(''); //Call test
+}
+
 // WAIT FOR THE PAGE TO LOAD BEFORE ADDING LISTENERS
 window.addEventListener('load', function () {
   document
@@ -49,4 +81,14 @@ window.addEventListener('load', function () {
   document
     .getElementById('cancel-pass-btn')
     .addEventListener('click', hidePassForm);
+
+  document
+    .getElementById('registerModal')
+    .querySelector('.creds-form-confirm')
+    .addEventListener('click', registerUser);
+
+  document
+    .getElementById('signinModal')
+    .querySelector('.creds-form-confirm')
+    .addEventListener('click', loginUser);
 });
